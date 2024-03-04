d6d241dd41595b181aa50e161a5dfb7227b57892
crawkatt <man849916@gmail.com>
2024-03-04 13:38:37 -0300

    🧪 test: Se han refactorizado los Tests para que sean minimamente funcionales (se debe arreglar el test b)

--- src/test.rs
+++ src/test.rs
@@ -3,6 +3,7 @@ pub mod tests {
     use serenity::all::{GuildId, RoleId, UserId};
     use surrealdb::engine::remote::ws::Ws;
     use surrealdb::opt::auth::Root;
+    use tokio::runtime::Runtime;
     use tokio::task;
     use crate::commands::setters::set_joke::Joke;
     use crate::DB;
@@ -41,143 +42,164 @@ pub mod tests {
         assert_eq!(result, Some("https://www.google.com".to_string()));
     }
 
-    #[tokio::test]
-    async fn b_handle_forbidden_user() {
-        setup().await;
+    #[test]
+    fn b_handle_forbidden_user() {
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async {
+            setup().await;
 
-        let guild_id = GuildId::new(1_014_327_651_772_674_168);
-        let forbidden_user_id = UserId::new(1_203_715_036_061_892_628);
+            let guild_id = GuildId::new(1_014_327_651_772_674_168);
+            let forbidden_user_id = UserId::new(1_203_715_036_061_892_628);
 
-        let forbidden_user_data = ForbiddenUserData::get_forbidden_user_id(guild_id).await.unwrap().unwrap();
-        let database_forbidden_user_id = UserId::new(forbidden_user_data);
+            let forbidden_user_data = ForbiddenUserData::get_forbidden_user_id(guild_id).await.unwrap().unwrap();
+            let database_forbidden_user_id = UserId::new(forbidden_user_data);
 
-        assert_eq!(database_forbidden_user_id, forbidden_user_id);
+            assert_eq!(database_forbidden_user_id, forbidden_user_id);
 
-        task::yield_now().await;
+            //task::yield_now().await;
+        });
     }
 
-    #[tokio::test]
-    async fn c_handle_forbidden_role() {
-        setup().await;
-        let guild_id = GuildId::new(1_014_327_651_772_674_168);
-        let forbidden_role_id = "1210266785061019658".to_string();
-        let forbidden_role_id_to_u64 = forbidden_role_id.parse::<u64>().unwrap();
+    #[test]
+    fn c_handle_forbidden_role() {
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async {
+            setup().await;
+            let guild_id = GuildId::new(1_014_327_651_772_674_168);
+            let forbidden_role_id = "1210266785061019658".to_string();
+            let forbidden_role_id_to_u64 = forbidden_role_id.parse::<u64>().unwrap();
 
-        let forbidden_role_data = ForbiddenRoleData::get_role_id(guild_id).await.unwrap().unwrap();
-        let database_forbidden_role_id = forbidden_role_data;
+            let forbidden_role_data = ForbiddenRoleData::get_role_id(guild_id).await.unwrap().unwrap();
+            let database_forbidden_role_id = forbidden_role_data;
 
-        assert_eq!(database_forbidden_role_id, forbidden_role_id_to_u64);
+            assert_eq!(database_forbidden_role_id, forbidden_role_id_to_u64);
 
-        task::yield_now().await;
+            //task::yield_now().await;
+        });
     }
 
-    #[tokio::test]
-    async fn d_handle_warn_system() {
-        setup().await;
+    #[test]
+    fn d_handle_warn_system() {
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async {
+            setup().await;
 
-        let user_id = UserId::new(395_631_548_629_516_298);
-        let mut warns = Warns::new(user_id);
-        let warns_data = warns.get_warns().await.unwrap().unwrap();
-        let warns_counter = warns_data.warns;
+            let user_id = UserId::new(395_631_548_629_516_298);
+            let mut warns = Warns::new(user_id);
+            let warns_data = warns.get_warns().await.unwrap().unwrap();
+            let warns_counter = warns_data.warns;
 
-        // Comprueba si las advertencias coinciden
-        warns.get_warns().await.unwrap().unwrap();
-        if (warns_counter..3).next().is_some() {
-            warns.add_warn().await.unwrap();
+            // Comprueba si las advertencias coinciden
+            warns.get_warns().await.unwrap().unwrap();
+            if (warns_counter..3).next().is_some() {
+                warns.add_warn().await.unwrap();
 
-            if warns_counter >= 3 {
-                warns.reset_warns().await.unwrap();
-                assert_eq!(warns.get_warns().await.unwrap().unwrap().warns, 0);
+                if warns_counter >= 3 {
+                    warns.reset_warns().await.unwrap();
+                    assert_eq!(warns.get_warns().await.unwrap().unwrap().warns, 0);
+                }
             }
-        }
 
-        task::yield_now().await;
+            //task::yield_now().await;
+        });
     }
 
-    #[tokio::test]
-    async fn e_handle_joke() {
-        setup().await;
+    #[test]
+    fn e_handle_joke() {
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async {
+            setup().await;
 
-        // Objeto Joke simulando ser obtenido desde la Base de Datos
-        let guild_id = GuildId::new(983_473_640_387_518_504);
-        let joke = Joke::get_joke_target_id(guild_id).await.unwrap();
+            // Objeto Joke simulando ser obtenido desde la Base de Datos
+            let guild_id = GuildId::new(983_473_640_387_518_504);
+            let joke = Joke::get_joke_target_id(guild_id).await.unwrap();
 
-        // user_id obtenido desde Discord
-        let user_id: u64 = 1_076_623_900_697_448_478;
+            // user_id obtenido desde Discord
+            let user_id: u64 = 1_076_623_900_697_448_478;
 
-        // Comparamos el target del objeto Joke con el user_id hardcodeado
-        assert_eq!(joke, user_id);
+            // Comparamos el target del objeto Joke con el user_id hardcodeado
+            assert_eq!(joke, user_id);
 
-        task::yield_now().await;
+            //task::yield_now().await;
+        });
     }
 
-    #[tokio::test]
-    async fn f_handle_joke_swtich() {
-        setup().await;
+    #[test]
+    fn f_handle_joke_swtich() {
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async {
+            setup().await;
 
-        let database_password = dotenvy::var("DATABASE_PASSWORD").expect("missing SURREAL_PASSWORD");
-        DB.signin(Root {
-            username: "root",
-            password: &database_password,
-        }).await.expect("Could not sign in");
+            let database_password = dotenvy::var("DATABASE_PASSWORD").expect("missing SURREAL_PASSWORD");
+            DB.signin(Root {
+                username: "root",
+                password: &database_password,
+            }).await.expect("Could not sign in");
 
-        // Objeto Joke simulando ser obtenido desde la Base de Datos
-        let guild_id = GuildId::new(983_473_640_387_518_504);
-        let joke = Joke::get_joke_status(guild_id).await.unwrap();
+            // Objeto Joke simulando ser obtenido desde la Base de Datos
+            let guild_id = GuildId::new(983_473_640_387_518_504);
+            let joke = Joke::get_joke_status(guild_id).await.unwrap();
 
-        let mut joke = Joke::new(guild_id.to_string(), joke, String::new());
+            let mut joke = Joke::new(guild_id.to_string(), joke, String::new());
 
-        if joke.is_active {
-            joke.switch(false).await.unwrap();
-            assert!(!joke.is_active);
-        } else {
-            joke.switch(true).await.unwrap();
-            assert!(joke.is_active);
-        }
+            if joke.is_active {
+                joke.switch(false).await.unwrap();
+                assert!(!joke.is_active);
+            } else {
+                joke.switch(true).await.unwrap();
+                assert!(joke.is_active);
+            }
 
-        task::yield_now().await;
+            //task::yield_now().await;
+        });
     }
 
-    #[tokio::test]
-    async fn g_check_admin_exception() {
-        setup().await;
+    #[test]
+    fn g_check_admin_exception() {
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async {
+            setup().await;
 
-        let guild_id = GuildId::new(1_014_327_651_772_674_168);
+            let guild_id = GuildId::new(1_014_327_651_772_674_168);
 
-        // El rol de administrador simulado desde la API de Discord
-        let admin_role_1_string = "1020156814152712222".to_string();
-        let admin_role_id_to_u64 = admin_role_1_string.parse::<u64>().unwrap();
-        let admin_role_id = RoleId::new(admin_role_id_to_u64);
+            // El rol de administrador simulado desde la API de Discord
+            let admin_role_1_string = "1020156814152712222".to_string();
+            let admin_role_id_to_u64 = admin_role_1_string.parse::<u64>().unwrap();
+            let admin_role_id = RoleId::new(admin_role_id_to_u64);
 
-        let get_role_id = AdminData::get_admin_role(guild_id).await.unwrap();
-        let role_to_u64 = get_role_id.unwrap().parse::<u64>().unwrap();
-        let database_role_id_1 = RoleId::new(role_to_u64);
+            let get_role_id = AdminData::get_admin_role(guild_id).await.unwrap();
+            let role_to_u64 = get_role_id.unwrap().parse::<u64>().unwrap();
+            let database_role_id_1 = RoleId::new(role_to_u64);
 
-        let admin_role_2_string = "1196523947177545858".to_string();
-        let admin_role_id_to_u64 = admin_role_2_string.parse::<u64>().unwrap();
-        let admin_role_id_2 = RoleId::new(admin_role_id_to_u64);
+            let admin_role_2_string = "1196523947177545858".to_string();
+            let admin_role_id_to_u64 = admin_role_2_string.parse::<u64>().unwrap();
+            let admin_role_id_2 = RoleId::new(admin_role_id_to_u64);
 
-        let get_role_id_2 = AdminData::get_admin_role_2(guild_id).await.unwrap();
-        let role_to_u64 = get_role_id_2.unwrap().parse::<u64>().unwrap();
-        let database_role_id_2 = RoleId::new(role_to_u64);
+            let get_role_id_2 = AdminData::get_admin_role_2(guild_id).await.unwrap();
+            let role_to_u64 = get_role_id_2.unwrap().parse::<u64>().unwrap();
+            let database_role_id_2 = RoleId::new(role_to_u64);
 
-        assert_eq!(database_role_id_1, admin_role_id);
-        assert_eq!(database_role_id_2, admin_role_id_2);
+            assert_eq!(database_role_id_1, admin_role_id);
+            assert_eq!(database_role_id_2, admin_role_id_2);
 
-        task::yield_now().await;
+            //task::yield_now().await;
+        });
     }
 
     // no hace falta usar namespace
-    #[tokio::test]
-    async fn h_get_blacklist_link() {
-        setup().await;
+    #[test]
+    fn h_get_blacklist_link() {
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async {
+            setup().await;
 
-        let guild_id = GuildId::new(1_014_327_651_772_674_168);
-        let link = "GET YOUR ADBOE PHOTOSHOP FREE NOW: https://www.youtube.com/watch?v=N-gcKsjVMp0 @everyone @here".to_string();
-        let extracted_link = extract_link(&link).unwrap();
-        let result = BlackListData::get_blacklist_link(guild_id, extracted_link.clone()).await.unwrap();
-        assert_eq!(result, extracted_link);
+            let guild_id = GuildId::new(1_014_327_651_772_674_168);
+            let link = "GET YOUR ADBOE PHOTOSHOP FREE NOW: https://www.youtube.com/watch?v=N-gcKsjVMp0 @everyone @here".to_string();
+            let extracted_link = extract_link(&link).unwrap();
+            let result = BlackListData::get_blacklist_link(guild_id, &extracted_link).await.unwrap();
+            assert_eq!(result, Some(extracted_link));
 
-        task::yield_now().await;
+            //task::yield_now().await;
+        });
     }
 }
