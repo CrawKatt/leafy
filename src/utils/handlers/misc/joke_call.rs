315fbf7f1a5c0fa2f06e06fdb0d3422856b1709f
crawkatt <man849916@gmail.com>
2024-03-04 13:40:33 -0300

    ♻️ refactor: Se ha refactorizado la función `handle_joke` para que sea más legible

--- src/utils/handlers/misc/joke_call.rs
+++ src/utils/handlers/misc/joke_call.rs
@@ -3,41 +3,25 @@ use serenity::all::{CreateAttachment, Message};
 use crate::commands::setters::set_joke::Joke;
 use poise::serenity_prelude as serenity;
 use crate::commands::setters::set_joke_channel::JokeChannelData;
-use crate::DB;
-use crate::utils::misc::debug::{UnwrapErrors, UnwrapLog};
-
-const CURRENT_MODULE: &str = file!();
+use crate::utils::misc::debug::UnwrapErrors;
 
 pub async fn handle_joke(mut joke: Joke, new_message: &Message, ctx: &serenity::Context) -> Result<(), UnwrapErrors> {
-    let sql_query = "SELECT * FROM joke_channel WHERE guild_id = $guild_id";
-    let joke_channel: Option<JokeChannelData> = DB
-        .query(sql_query)
-        .bind(("guild_id", &joke.guild_id)) // pasar el valor
-        .await?
-        .take(0)?;
-
-    let joke_channel = joke_channel
-        .unwrap_log("No se ha establecido un canal de broma", CURRENT_MODULE, line!())?
-        .channel_id
-        .parse::<u64>()?;
-
+    let joke_channel = JokeChannelData::get_joke_channel(&joke.guild_id).await?;
     let joke_channel = serenity::all::ChannelId::new(joke_channel);
-
     let joke_id = joke.target.parse::<u64>()?;
     let joke_status = joke.is_active;
+    let author_user_id = new_message.author.id;
 
-    if joke_status && joke_channel == new_message.channel_id {
-        let author_user_id = new_message.author.id;
-        if author_user_id == joke_id {
-            let mut message_map = HashMap::new();
-            message_map.insert("content", " ".to_string());
-            let http = ctx.http.clone();
-            let attachment = CreateAttachment::path("./assets/joke.gif").await?;
-            http.send_message(new_message.channel_id, vec![attachment], &message_map).await?;
+    if !joke_status || joke_channel != new_message.channel_id { return Ok(()) }
+    if author_user_id != joke_id { return Ok(()) }
 
-            joke.switch(false).await?;
-        }
-    }
+    let mut message_map = HashMap::new();
+    message_map.insert("content", " ".to_string());
+    let http = ctx.http.clone();
+    let attachment = CreateAttachment::path("./assets/joke.gif").await?;
+    http.send_message(new_message.channel_id, vec![attachment], &message_map).await?;
+
+    joke.switch(false).await?;
 
     Ok(())
 }
