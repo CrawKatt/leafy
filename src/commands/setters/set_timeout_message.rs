879da1baefd200b1c565a9d5134dfb2583e59a65
crawkatt <man849916@gmail.com>
2024-05-03 17:46:38 -0400

    💥 breaking_update: Se ha aplicado la reestructuración de la Base de Datos en `set_timeout_message.rs`

--- src/commands/setters/set_timeout_message.rs
+++ src/commands/setters/set_timeout_message.rs
@@ -1,8 +1,6 @@
 use crate::utils::{CommandResult, Context};
-use crate::utils::misc::debug::UnwrapLog;
-use crate::commands::setters::TimeOutMessageData;
+use crate::utils::misc::config::{GuildData, Messages};
 
-/// Establece el mensaje de time out
 #[poise::command(
     prefix_command,
     slash_command,
@@ -15,17 +13,25 @@ pub async fn set_time_out_message(
     ctx: Context<'_>,
     #[description = "The message to set as the time out message"] time_out_message: String,
 ) -> CommandResult {
-    let guild_id = ctx.guild_id().unwrap_log("Failed to get guild id", module_path!(), line!())?;
-    let time_out_message_data = TimeOutMessageData::new(guild_id, time_out_message.clone());
-    let existing_data = time_out_message_data.verify_data().await?;
+    let guild_id = ctx.guild_id().unwrap();
+    
+    let existing_data = GuildData::verify_data(guild_id).await?;
+    if existing_data.is_none() {
+        let data = GuildData::default()
+            .guild_id(guild_id)
+            .messages_config(Messages::default()
+                .time_out(&time_out_message));
 
-    let Some(existing_data) = existing_data else {
-        time_out_message_data.save_to_db().await?;
+        data.save_to_db().await?;
         ctx.say(format!("Time out message establecido: {time_out_message}")).await?;
-        return Ok(())
-    };
 
-    existing_data.update_in_db().await?;
+        return Ok(())
+    }
+
+    let data = Messages::default()
+        .time_out(&time_out_message);
+
+    data.update_field_in_db("messages_config.time_out", &time_out_message, &guild_id.to_string()).await?;
     ctx.say(format!("Time out message actualizado: {time_out_message}")).await?;
 
     Ok(())
