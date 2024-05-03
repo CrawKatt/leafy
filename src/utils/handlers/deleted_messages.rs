5ea1fd43a4a27ecbb98e300d7bcbfe92a8f192ab
crawkatt <man849916@gmail.com>
2024-05-03 17:25:29 -0400

    💥 breaking_update: Se ha aplicado la reestructuración de la Base de Datos en `deleted_messages.rs`

--- src/utils/handlers/deleted_messages.rs
+++ src/utils/handlers/deleted_messages.rs
@@ -3,19 +3,15 @@ use reqwest::Url;
 use serenity::all::{ChannelId, MessageId};
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
+use crate::utils::misc::config::GuildData;
 use crate::utils::CommandResult;
 use crate::utils::MessageData;
-use crate::commands::setters::GuildData;
-use crate::utils::misc::debug::UnwrapLog;
+use crate::utils::misc::debug::{IntoUnwrapResult, UnwrapLog};
 use crate::utils::misc::embeds::{send_embed, send_embed_with_attachment};
 
 pub async fn delete_message_handler(ctx: &serenity::Context, channel_id: &ChannelId, deleted_message_id: &MessageId) -> CommandResult {
-
     let database_info = MessageData::get_message_data(deleted_message_id).await?;
-    let Some(database_message) = database_info else {
-        return Ok(())
-    };
-
+    let Some(database_message) = database_info else { return Ok(()) };
     let message_content = database_message.message_content.clone();
     let message_channel_id = database_message.channel_id;
     let author_id = database_message.author_id;
@@ -29,14 +25,15 @@ pub async fn delete_message_handler(ctx: &serenity::Context, channel_id: &Channe
     }
 
     // Obtener el canal de logs de la base de datos
-    let result_database = database_message.guild_id.unwrap_or_default();
-    let log_channel_id = GuildData::get_log_channel(result_database).await?;
-
-    let log_channel = log_channel_id.unwrap_or_default().log_channel_id;
-    if channel_id == &log_channel {
-        return Ok(());
-    }
-
+    let result_database = database_message.guild_id.unwrap_log("No se pudo obtener el id del servidor", file!(), line!())?;
+    let log_channel = GuildData::verify_data(result_database).await?
+        .into_result()?
+        .channel_config
+        .log_channel_id
+        .into_result()?
+        .parse::<ChannelId>()?;
+    
+    if channel_id == &log_channel { return Ok(()) }
     send_embed(ctx, result_database, log_channel, &message_channel_id, author_id, &message_content).await?;
 
     Ok(())
@@ -44,9 +41,7 @@ pub async fn delete_message_handler(ctx: &serenity::Context, channel_id: &Channe
 
 async fn handle_audio(ctx: &serenity::Context, deleted_message_id: &MessageId, database_message: &MessageData, channel_id: &ChannelId) -> CommandResult {
     let audio_info = MessageData::get_audio_data(deleted_message_id).await?;
-    let Some(audio_info) = audio_info else {
-        return Ok(());
-    };
+    let Some(audio_info) = audio_info else { return Ok(()) };
 
     // Descargar el archivo de audio
     let attachment_url = audio_info.message_content;
@@ -58,12 +53,14 @@ async fn handle_audio(ctx: &serenity::Context, deleted_message_id: &MessageId, d
     out.write_all(&bytes).await?;
 
     let result_database = database_message.guild_id.unwrap_log("No se pudo obtener el id del servidor", file!(), line!())?;
-    let log_channel_id = GuildData::get_log_channel(result_database).await?;
-    let log_channel = log_channel_id.unwrap_or_default().log_channel_id;
-    if channel_id == &log_channel {
-        return Ok(());
-    }
-
+    let log_channel = GuildData::verify_data(result_database).await?
+        .into_result()?
+        .channel_config
+        .log_channel_id
+        .into_result()?
+        .parse::<ChannelId>()?;
+    
+    if channel_id == &log_channel { return Ok(()) }
     send_embed_with_attachment(ctx, result_database, log_channel, &database_message.channel_id, database_message.author_id, &filename).await?;
 
     Ok(())
