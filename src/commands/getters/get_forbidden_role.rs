df70a73d0b2fd3306cfa15cd83877f8044b2dbad
crawkatt <man849916@gmail.com>
2024-05-03 17:29:09 -0400

    💥 breaking_update: Se ha aplicado la reestructuración de la Base de Datos en `get_forbidden_role.rs`

--- src/commands/getters/get_forbidden_role.rs
+++ src/commands/getters/get_forbidden_role.rs
@@ -1,6 +1,6 @@
 use crate::DB;
+use crate::utils::misc::config::GuildData;
 use crate::utils::{CommandResult, Context};
-use crate::commands::setters::ForbiddenRoleData;
 
 /// Obtiene el rol que está prohibido de mencionar si está establecido.
 #[poise::command(
@@ -17,8 +17,8 @@ pub async fn get_forbidden_role(
 
     DB.use_ns("discord-namespace").use_db("discord").await?;
     let guild_id = ctx.guild_id().unwrap();
-    let sql_query = "SELECT * FROM forbidden_roles WHERE guild_id = $guild_id";
-    let database_info: Option<ForbiddenRoleData> = DB
+    let sql_query = "SELECT * FROM guild_config WHERE guild_id = $guild_id";
+    let database_info: Option<GuildData> = DB
         .query(sql_query)
         .bind(("guild_id", guild_id)) // pasar el valor
         .await?
@@ -28,8 +28,13 @@ pub async fn get_forbidden_role(
         ctx.say("No se ha establecido un rol prohíbido de mencionar").await?;
         return Ok(())
     };
-
-    let forbidden_role_id = forbidden_role_id.role_id.parse::<u64>()?;
+    
+    let forbidden_role_id = forbidden_role_id
+        .forbidden_config
+        .role_id
+        .ok_or("No se ha establecido un rol prohíbido de mencionar")?
+        .parse::<u64>()?;
+    
     let forbidden_role = ctx.cache().role(guild_id, forbidden_role_id).ok_or("Role not found")?.name.clone();
 
     ctx.say(format!("Forbidden role is **{forbidden_role}**")).await?;
