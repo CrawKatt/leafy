2f3b9739aa531d68e34716caf545cfc2fb50da50
crawkatt <man849916@gmail.com>
2024-05-03 17:50:45 -0400

    💥 breaking_update: Se ha aplicado la reestructuración de la Base de Datos en `set_welcome_message.rs`

--- src/commands/setters/set_welcome_message.rs
+++ src/commands/setters/set_welcome_message.rs
@@ -1,63 +1,6 @@
 use crate::DB;
 use crate::utils::{CommandResult, Context};
-use poise::serenity_prelude as serenity;
-use serde::{Deserialize, Serialize};
-use surrealdb::Result as SurrealResult;
-
-#[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct WelcomeMessageData {
-    pub guild_id: String,
-    pub message: String,
-}
-
-impl WelcomeMessageData {
-    pub fn new(
-        guild_id: serenity::GuildId,
-        message: String,
-    ) -> Self {
-        Self {
-            guild_id: guild_id.to_string(),
-            message,
-        }
-    }
-
-    /// Guarda el link en la base de datos
-    pub async fn save_to_db(&self) -> SurrealResult<()> {
-        DB.use_ns("discord-namespace").use_db("discord").await?;
-        let _created: Vec<Self> = DB
-            .create("welcome_message")
-            .content(self)
-            .await?;
-
-        Ok(())
-    }
-
-    pub async fn update_to_db(&self) -> SurrealResult<()> {
-        DB.use_ns("discord-namespace").use_db("discord").await?;
-        let sql_query = "UPDATE welcome_message SET message = $message WHERE guild_id = $guild_id";
-        let _updated: Vec<Self> = DB
-            .query(sql_query)
-            .bind(("message", self.message.to_string()))
-            .bind(("guild_id", self.guild_id.to_string()))
-            .await?
-            .take(0)?;
-
-        Ok(())
-    }
-
-    /// Verifica si el link ya se encuentra en la base de datos
-    pub async fn verify_data(&self) -> SurrealResult<Option<Self>> {
-        DB.use_ns("discord-namespace").use_db("discord").await?;
-        let sql_query = "SELECT * FROM welcome_message WHERE guild_id = $guild_id";
-        let existing_data: Option<Self> = DB
-            .query(sql_query)
-            .bind(("guild_id", self.guild_id.to_string()))
-            .await?
-            .take(0)?;
-
-        Ok(existing_data)
-    }
-}
+use crate::utils::misc::config::{GuildData, Messages};
 
 #[poise::command(
     prefix_command,
@@ -71,19 +14,27 @@ pub async fn set_welcome_message(
     ctx: Context<'_>,
     #[description = "Mensaje de bienvenida"] message: String,
 ) -> CommandResult {
+    DB.use_ns("discord-namespace").use_db("discord").await?;
     let guild_id = ctx.guild_id().unwrap();
-    let data = WelcomeMessageData::new(guild_id, message);
-    let existing_data = data.verify_data().await?;
+    let existing_data = GuildData::verify_data(guild_id).await?;
 
-    if existing_data.is_some() {
-        data.update_to_db().await?;
-        poise::say_reply(ctx, "Mensaje de bienvenida actualizado").await?;
+    if existing_data.is_none() {
+        let data = GuildData::default()
+            .guild_id(guild_id)
+            .messages_config(Messages::default()
+                .welcome(&message));
+
+        data.save_to_db().await?;
+        ctx.say(format!("El mensaje de bienvenida ha sido establecido a: {message}")).await?;
 
         return Ok(())
     }
 
-    data.save_to_db().await?;
-    poise::say_reply(ctx, "Mensaje de bienvenida guardado").await?;
+    let data = Messages::default()
+        .welcome(&message);
+
+    data.update_field_in_db("messages_config.welcome", &message, &guild_id.to_string()).await?;
+    ctx.say(format!("El mensaje de bienvenida ha sido actualizado a: {message}")).await?;
 
     Ok(())
 }
