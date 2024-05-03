08861590f017dbaed250e3428f00a9cae5d7fe33
crawkatt <man849916@gmail.com>
2024-05-03 17:54:39 -0400

    ♻️ refactor: Se ha removido el Path prefix innecesario

--- src/utils/misc/autocomplete.rs
+++ src/utils/misc/autocomplete.rs
@@ -11,7 +11,7 @@ pub async fn args_log_command<'a>(
 ) -> impl Stream<Item = String> + 'a {
     futures::stream::iter(["channel"])
         .filter(move |name| futures::future::ready(name.starts_with(partial)))
-        .map(std::string::ToString::to_string)
+        .map(ToString::to_string)
 }
 pub async fn args_set_forbidden_user<'a>(
     _ctx: Context<'_>,
@@ -19,7 +19,7 @@ pub async fn args_set_forbidden_user<'a>(
 ) -> impl Stream<Item = String> + 'a {
     futures::stream::iter(["channel"])
         .filter(move |name| futures::future::ready(name.starts_with(partial)))
-        .map(std::string::ToString::to_string)
+        .map(ToString::to_string)
 }
 
 pub async fn args_set_forbidden_role<'a>(
@@ -28,7 +28,7 @@ pub async fn args_set_forbidden_role<'a>(
 ) -> impl Stream<Item = String> + 'a {
     futures::stream::iter(["role"])
         .filter(move |name| futures::future::ready(name.starts_with(partial)))
-        .map(std::string::ToString::to_string)
+        .map(ToString::to_string)
 }
 
 pub async fn args_set_timeout_timer<'a>(
@@ -37,7 +37,7 @@ pub async fn args_set_timeout_timer<'a>(
 ) -> impl Stream<Item = String> + 'a {
     futures::stream::iter(["1 Minuto", "5 Minutos", "30 Minutos", "60 Minutos", "1 Semana"])
         .filter(move |name| futures::future::ready(name.starts_with(partial)))
-        .map(std::string::ToString::to_string)
+        .map(ToString::to_string)
 }
 
 pub async fn args_set_admins<'a>(
@@ -46,5 +46,5 @@ pub async fn args_set_admins<'a>(
 ) -> impl Stream<Item = String> + 'a {
     futures::stream::iter(["user"])
         .filter(move |name| futures::future::ready(name.starts_with(partial)))
-        .map(std::string::ToString::to_string)
+        .map(ToString::to_string)
 }
