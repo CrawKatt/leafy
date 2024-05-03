03eb4c1c0b4121ba1f7786b3c93304a5c00f3482
crawkatt <man849916@gmail.com>
2024-05-03 17:44:06 -0400

    💥 breaking_update: Se ha aplicado la reestructuración de la Base de Datos en `set_log_channel.rs`

--- src/commands/setters/set_log_channel.rs
+++ src/commands/setters/set_log_channel.rs
@@ -1,11 +1,10 @@
 use serenity::all::Channel;
-use crate::DB;
-use crate::utils::misc::autocomplete::args_log_command;
-use crate::utils::{CommandResult, Context};
-use crate::utils::misc::debug::UnwrapLog;
-use crate::commands::setters::GuildData;
 
-/// Establece el canal de logs del servidor
+use crate::DB;
+use crate::utils::{CommandResult, Context};
+use crate::utils::misc::autocomplete::args_log_command;
+use crate::utils::misc::config::{Channels, GuildData};
+
 #[poise::command(
     prefix_command,
     slash_command,
@@ -16,26 +15,32 @@ use crate::commands::setters::GuildData;
 )]
 pub async fn set_log_channel(
     ctx: Context<'_>,
+    #[description = "The channel to set as the log channel"]
     #[autocomplete = "args_log_command"]
-    #[description = "The channel to set as the log channel"] channel: Channel,
+    log_channel: Channel,
 ) -> CommandResult {
     DB.use_ns("discord-namespace").use_db("discord").await?;
+    let guild_id = ctx.guild_id().unwrap();
+    let channel_id = log_channel.id().to_string();
 
-    let current_module = file!();
-    let guild_id = ctx.guild_id().unwrap_log("Could not get guild_id", current_module, line!())?;
-    let channel_id = channel.id();
-    let data = GuildData::new(guild_id, channel_id);
-    let existing_data = data.verify_data().await?;
+    let existing_data = GuildData::verify_data(guild_id).await?;
 
-    let Some(_) = existing_data else {
-        // Si el dato no existe, créalo
+    if existing_data.is_none() {
+        let data = GuildData::default()
+            .guild_id(guild_id)
+            .channel_config(Channels::default()
+                .log_channel_id(&channel_id)
+            );
         data.save_to_db().await?;
         ctx.say(format!("Log channel establecido: <#{channel_id}>")).await?;
-        return Ok(());
-    };
 
-    // Si el dato ya existe, actualízalo
-    data.update_in_db().await?;
+        return Ok(());
+    }
+
+    let data = Channels::default()
+        .log_channel_id(&channel_id);
+
+    data.update_field_in_db("channel_config.log_channel_id", &channel_id, &guild_id.to_string()).await?;
     ctx.say(format!("Log channel establecido: <#{channel_id}>")).await?;
 
     Ok(())
