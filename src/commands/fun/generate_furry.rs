87fc7efb479bf5de4262f079768ee82f2e365342
crawkatt <man849916@gmail.com>
2024-06-01 14:24:41 -0400

    🎨 improve: Se ha corregido el código repetitivo para ciertas tareas en funciones

--- src/commands/fun/generate_furry.rs
+++ src/commands/fun/generate_furry.rs
@@ -3,11 +3,12 @@ use std::fs::remove_file;
 use image::DynamicImage;
 use plantita_welcomes::create_welcome::combine_images;
 use reqwest::get;
-use serenity::all::{CreateMessage, GetMessages, Member, UserId};
+use serenity::all::{CreateMessage, Member, UserId};
 use serenity::builder::CreateAttachment;
 
+use crate::commands::fun::get_target_info;
 use crate::utils::{CommandResult, Context};
-use crate::utils::debug::{IntoUnwrapResult, UnwrapResult};
+use crate::utils::debug::UnwrapResult;
 
 #[poise::command(
     prefix_command,
@@ -16,40 +17,14 @@ use crate::utils::debug::{IntoUnwrapResult, UnwrapResult};
     track_edits
 )]
 pub async fn furry(ctx: Context<'_>, target: Option<Member>) -> CommandResult {
-    let guild_id = ctx.guild_id().unwrap(); // SAFETY: Si el mensaje no es de un servidor, no se ejecutará el comando
-
-    if target.is_some() {
-        let target_member = target.into_result()?;
-        let target_avatar = target_member.face(); // el método face devuelve el avatar si existe, de lo contrario, el avatar predeterminado
-        let target_id = &target_member.user.id;
-        let channel_id = ctx.channel_id();
-        let mut background = image::open("./assets/furry_backgorund.jpg")?;
-        let file = generate_furry(&mut background, target_avatar, target_id, 550, 280, 250).await?;
-        let attachment = CreateAttachment::path(&file).await?;
-
-        channel_id.send_files(&ctx.http(), vec![attachment], CreateMessage::default()).await?;
-        remove_file(file)?;
-
-        return Ok(())
-    }
-
-    let messages = ctx.channel_id().messages(&ctx.http(), GetMessages::default()).await?;
-    let message = messages
-        .first()
-        .into_result()?;
-
-    let target_id = &message.referenced_message.as_ref().into_result()?.author.id;
-    let target_member = guild_id.member(&ctx.http(), target_id).await?;
-    let target_avatar = target_member.face(); // el método face devuelve el avatar si existe, de lo contrario, el avatar predeterminado
+    let (target_id, target_avatar) = get_target_info(&ctx, target).await?;
     let channel_id = ctx.channel_id();
-
-    let mut background = image::open("./assets/furry_backgorund.jpg").unwrap();
-    let file = generate_furry(&mut background, target_avatar, target_id, 550, 280, 250).await?;
-
+    let mut background = image::open("./assets/furry_backgorund.jpg")?;
+    let file = generate_furry(&mut background, target_avatar, &target_id, 550, 280, 250).await?;
     let attachment = CreateAttachment::path(&file).await?;
-    channel_id.send_files(&ctx.http(), vec![attachment], CreateMessage::default()).await?;
 
-    remove_file(file)?;
+    channel_id.send_files(&ctx.http(), vec![attachment], CreateMessage::default()).await?;
+    remove_file(&file)?;
 
     Ok(())
 }
--- src/commands/fun/mod.rs
+++ src/commands/fun/mod.rs
@@ -1,3 +1,24 @@
+use serenity::all::{GetMessages, Member, UserId};
+use crate::utils::Context;
+use crate::utils::debug::{IntoUnwrapResult, UnwrapResult};
+
 pub mod generate_furry;
 pub mod screenshot_this;
 pub mod generate_pride;
+
+pub async fn get_target_info(ctx: &Context<'_>, target: Option<Member>) -> UnwrapResult<(UserId, String)> {
+    let guild_id = ctx.guild_id().into_result()?; // SAFETY: Si el mensaje no es de un servidor, no se ejecutará el comando
+
+    let Some(target_member) = target else {
+        let messages = ctx.channel_id().messages(&ctx.http(), GetMessages::default()).await?;
+        let message = messages.first().into_result()?;
+        let target_id = message.referenced_message.as_ref().into_result()?.author.id;
+        let target_member = guild_id.member(&ctx.http(), &target_id).await?;
+        let target_avatar = target_member.face(); // el método face devuelve el avatar si existe, de lo contrario, el avatar predeterminado
+        return Ok((target_id, target_avatar))
+    };
+
+    let target_avatar = target_member.face(); // el método face devuelve el avatar si existe, de lo contrario, el avatar predeterminado
+    let target_id = target_member.user.id;
+    Ok((target_id, target_avatar))
+}
