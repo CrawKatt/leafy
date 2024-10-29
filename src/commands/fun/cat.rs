6c381015f063c5776a18016f8e5895f51d65d1b9
crawkatt <man849916@gmail.com>
2024-10-28 23:01:17 -0300

    🐛 fix: Corrige un Bug de tipos y mejora el formato del mensaje

--- src/commands/fun/cat.rs
+++ src/commands/fun/cat.rs
@@ -1,4 +1,4 @@
-use serenity::all::{CreateAttachment, CreateMessage, UserId};
+use serenity::all::{CreateAttachment, CreateMessage, Member, UserId};
 use crate::location;
 use crate::utils::{CommandResult, Context};
 use crate::utils::config::GuildData;
@@ -6,16 +6,17 @@ use crate::utils::debug::{IntoUnwrapResult, UnwrapLog};
 
 #[poise::command(
     prefix_command,
+    slash_command,
     category = "Fun",
     aliases("shh"),
     guild_only,
     track_edits
 )]
-pub async fn cat_shh(ctx: Context<'_>, user: Option<UserId>) -> CommandResult {
+pub async fn cat_shh(ctx: Context<'_>, user: Option<Member>) -> CommandResult {
     let channel_id = ctx.channel_id();
-    let user = user.unwrap_or_else(|| ctx.author().id);
-
-    let user = user.to_user(&ctx).await?;
+    let author = &*ctx.author_member().await.into_result()?;
+    let member = user.unwrap_or_else(|| author.clone());
+    let user_id = member.user.id;
 
     // obtener el usuario prohibido
     let forbidden_user = GuildData::verify_data(ctx.guild_id().unwrap()).await?
@@ -25,13 +26,13 @@ pub async fn cat_shh(ctx: Context<'_>, user: Option<UserId>) -> CommandResult {
         .unwrap_log(location!())?
         .parse::<UserId>()?;
 
-    if user.id == forbidden_user {
+    if user_id == forbidden_user {
         poise::say_reply(ctx, "No puedes usar este comando con un usuario al que no está permitido hacer mención (@)").await?;
         return Ok(())
     }
 
     let attachment = CreateAttachment::path("assets/shh.gif").await?;
-    channel_id.send_files(ctx.http(), vec![attachment], CreateMessage::default()).await?;
+    channel_id.send_files(ctx.http(), vec![attachment], CreateMessage::default().content(format!("<@{user_id}>"))).await?;
 
     Ok(())
 }
